73f13914200924c4952b72376c553736dc3bce31
Eveheeero <xhve00000@naver.com>
2022-10-18 03:02:54 +0900

    PE파일의 entry에 대한 파싱코드 작성

--- fireball/src/tests/pe_hello_world.rs
+++ fireball/src/tests/pe_hello_world.rs
@@ -6,3 +6,24 @@ fn pe_hello_world() {
     let pe = PE::from_binary(binary.to_vec()).unwrap();
     dbg!(pe);
 }
+
+#[test]
+fn pe_hello_world_entry_parse() {
+    let binary = include_bytes!("../../tests/resources/hello_world.exe");
+    let pe = PE::from_binary(binary.to_vec()).unwrap();
+    let gl = goblin::pe::PE::parse(binary).unwrap();
+    let mut entry_of_raw = 0;
+    for section in gl.sections {
+        let entry = gl.entry;
+        let section_start = section.virtual_address as usize;
+        let section_end = section.virtual_address as usize + section.virtual_size as usize;
+        if entry >= section_start && entry < section_end {
+            entry_of_raw = entry - section_start + section.pointer_to_raw_data as usize;
+            break;
+        }
+    }
+    let insts = pe.parse_asm(entry_of_raw, 0x60).unwrap();
+    for inst in insts.iter() {
+        println!("{line}", line = inst.to_string());
+    }
+}
