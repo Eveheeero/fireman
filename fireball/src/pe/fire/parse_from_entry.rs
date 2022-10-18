04d54c4ebe0f24af25e4c1bac7b83eeaaa889cfc
Eveheeero <xhve00000@naver.com>
2022-10-18 22:06:34 +0900

    parse_from_entry 일부작성

--- fireball/src/pe/fire/parse_from_entry.rs
+++ fireball/src/pe/fire/parse_from_entry.rs
@@ -1,8 +1,15 @@
 use super::PE;
+use crate::core::Address;
 
 impl PE {
     pub(super) fn _parse_from_entry(&self) -> Result<(), Box<dyn std::error::Error>> {
-        todo!();
+        let gl = goblin::pe::PE::parse(&self.binary)?;
+        let entry = Address::from_file_offset(&self.binary, gl.entry);
+
+        let insts = self.parse_range(entry, 0x100).unwrap();
+
+        dbg!(insts);
+
         Ok(())
     }
 }
