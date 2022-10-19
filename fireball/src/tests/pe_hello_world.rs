17e064d58d9a64a6004d118c89fe00ddde836bec
Eveheeero <xhve00000@naver.com>
2022-10-19 21:21:06 +0900

    pe파일에 대해, 가상주소기반 어셈블리 파싱과 파일 오프셋 기반 어셈블리 파싱 비교 테스트 작성

--- fireball/src/tests/pe_hello_world.rs
+++ fireball/src/tests/pe_hello_world.rs
@@ -15,9 +15,36 @@ fn pe_hello_world_entry_parse() {
     let binary = include_bytes!("../../tests/resources/hello_world.exe");
     let pe = PE::from_binary(binary.to_vec()).unwrap();
     let gl = goblin::pe::PE::parse(binary).unwrap();
-    let entry_of_raw = Address::from_file_offset(&binary.to_vec(), gl.entry as u64);
-    let insts = pe.parse_assem_range(entry_of_raw, 0x60).unwrap();
-    for inst in insts.iter() {
-        println!("{line}", line = inst.to_string());
+
+    // 가상주소 기반 어셈블리 파싱 확인
+    let entry_of_virtual_address = Address::from_virtual_address(&binary.to_vec(), gl.entry as u64);
+    let insts_by_virtual_address = pe
+        .parse_assem_range(entry_of_virtual_address, 0x60)
+        .unwrap();
+
+    // 엔트리 포인트의 파일 오프셋 연산 (2022-10-19 기준 0x725)
+    let mut entry_of_file_offset = 0;
+    for section in gl.sections {
+        if section.virtual_address as u64 <= gl.entry as u64
+            && gl.entry as u64 <= (section.virtual_address + section.virtual_size) as u64
+        {
+            entry_of_file_offset = gl.entry as u64 - section.virtual_address as u64
+                + section.pointer_to_raw_data as u64;
+            break;
+        }
+    }
+
+    // 파일 오프셋 기반 어셈블리 파싱 확인
+    let entry_of_file_offset = Address::from_file_offset(&binary.to_vec(), entry_of_file_offset);
+    let insts_by_file_offset = pe.parse_assem_range(entry_of_file_offset, 0x60).unwrap();
+
+    // 두 결과값이 일치하는지 확인
+    for (left, right) in insts_by_virtual_address
+        .iter()
+        .zip(insts_by_file_offset.iter())
+    {
+        assert_eq!(left.address(), right.address());
+        assert_eq!(left.mnemonic(), right.mnemonic());
+        assert_eq!(left.op_str(), right.op_str());
     }
 }
