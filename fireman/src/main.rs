715499208c8ecec73763c4f3bb8b83ab6cadfc95
Eveheeero <xhve00000@naver.com>
2022-10-20 08:59:58 +0900

    fireman에 대해 clap 적용

--- fireman/src/main.rs
+++ fireman/src/main.rs
@@ -1,7 +1,18 @@
 use fireball::Fireball;
 
+use clap::Parser;
+
+/// 디컴파일러 CLI 도구입니다.
+/// 현재 개발중입니다.
+#[derive(Parser)]
+struct Args {
+    /// 파일 경로
+    #[arg(short, long)]
+    path: String,
+}
+
 fn main() {
-    let target = std::env::args().nth(1).unwrap();
-    let fire = Fireball::from_path(&target).unwrap();
+    let args = Args::parse();
+    let fire = Fireball::from_path(&args.path).unwrap();
     dbg!(fire);
 }
