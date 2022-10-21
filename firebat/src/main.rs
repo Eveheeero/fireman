0ad5a16fc33dca14b3d3164e0b20d74c4d8c1cf4
Eveheeero <xhve00000@naver.com>
2022-10-21 12:12:25 +0900

    Firebat 추가

---
+++ firebat/src/main.rs
@@ -0,0 +1,29 @@
+#![windows_subsystem = "windows"]
+
+use eframe::egui;
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Firebat",
+        native_options,
+        Box::new(|cc| Box::new(FirebatApp::new(cc))),
+    );
+}
+
+#[derive(Default)]
+struct FirebatApp {}
+
+impl FirebatApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for FirebatApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
+}
