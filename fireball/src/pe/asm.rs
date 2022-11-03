d58802c5e33ecdff85b977aa3d17a9e2464cefc1
Eveheeero <xhve00000@naver.com>
2022-11-03 22:58:52 +0900

    PE구조체의 어셈블리 N개 파싱 작성

--- fireball/src/pe/asm.rs
+++ fireball/src/pe/asm.rs
@@ -4,13 +4,32 @@ use crate::core::Address;
 use capstone::Instructions;
 
 impl PE {
-    /// 어셈블리 코드를 파싱한다.
+    /// 범위만큼의 어셈블리 코드를 파싱한다.
     pub(crate) fn parse_assem_range(&self, offset: Address, size: u64) -> Result<Instructions, ()> {
         let file_offset = offset.get_file_offset();
         let virtual_offset = offset.get_virtual_address();
         let insns = match self.capstone.disasm_all(
             &self.binary[file_offset as usize..(file_offset + size) as usize],
             virtual_offset as u64,
+        ) {
+            Ok(insts) => insts,
+            Err(_) => return Err(()),
+        };
+        Ok(insns)
+    }
+
+    /// 어셈블리 코드를 N개 파싱한다.
+    pub(crate) fn parse_assen_count(
+        &self,
+        offset: Address,
+        count: usize,
+    ) -> Result<Instructions, ()> {
+        let file_offset = offset.get_file_offset();
+        let virtual_offset = offset.get_virtual_address();
+        let insns = match self.capstone.disasm_count(
+            &self.binary[file_offset as usize..],
+            virtual_offset as u64,
+            count,
         ) {
             Ok(insts) => insts,
             Err(_) => return Err(()),
