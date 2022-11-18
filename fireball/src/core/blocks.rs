b1e807ac0d918a1685f56e0065d3a2caa9efa740
Eveheeero <xhve00000@naver.com>
2022-11-18 13:10:51 +0900

    Blocks에 대한 주석 일부 개선

--- fireball/src/core/blocks.rs
+++ fireball/src/core/blocks.rs
@@ -2,26 +2,32 @@ use std::sync::Arc;
 
 use super::{Address, Block, Section};
 
+/// 블록들을 가지고 있는 구조체
 pub struct Blocks {
     data: std::sync::RwLock<std::collections::HashSet<Arc<Block>>>,
 }
 
 impl Blocks {
+    /// 블록들을 가지고 있는 구조체를 생성한다.
     pub(crate) fn new() -> Arc<Self> {
         Arc::new(Self {
             data: Default::default(),
         })
     }
 
-    pub(crate) fn new_block(
+    /// 새 블록을 추가한다.
+    /// 새로 만들어진 블록은 해당 구조체 내부에 저장되며, 복사본을 반환한다.
+    pub(crate) fn generate_block(
         &self,
         section: Arc<Section>,
         start_address_virtual: Address,
         end_address_virtual: Option<Address>,
         name: Option<String>,
     ) -> Arc<Block> {
+        /* 저장소의 락 해제 */
         let blocks_writer = &mut self.data.write().unwrap();
 
+        /* 주어진 정보로 새 블록 생성 */
         let new_block = Block::new(
             blocks_writer.len(),
             name,
@@ -30,14 +36,19 @@ impl Blocks {
             section,
         );
 
+        /* 새 블록을 저장소에 저장 */
         blocks_writer.insert(new_block.clone());
 
+        /* 반환 */
         new_block
     }
 
-    pub(crate) fn find_from_address(&self, address: Address) -> Option<Arc<Block>> {
+    /// 시작지점이 N인 블록 객체를 반환한다.
+    pub(crate) fn find_from_start_address(&self, address: Address) -> Option<Arc<Block>> {
+        /* 저장소의 락 해제 */
         let blocks_reader = &self.data.read().unwrap();
 
+        /* 저장소의 데이터에서 검사 */
         blocks_reader
             .iter()
             .find(|block| block.get_start_address_virtual() == &address)
--- fireball/src/pe/fire/parse_block.rs
+++ fireball/src/pe/fire/parse_block.rs
@@ -5,13 +5,20 @@ use crate::core::{Address, Block, Relation};
 
 impl PE {
     pub(super) fn _parse_block(&self, address: Address) -> (Arc<Block>, Option<Arc<Relation>>) {
+        /* 기본정보 파싱 및 변수 선언 */
+        // 블록이 들어갈 섹션
         let section = self
             .sections
             .from_virtual_address(address.get_virtual_address())
             .unwrap();
+        // 블록의 시작주소
         let block_start = address.clone();
+        // 블록의 끝 주소
         let block_end: Address;
+        // 블록이 가지고 있는, 다른 블록과의 관계
         let relation: Option<Arc<Relation>>;
+
+        /* 한 줄씩 인스트럭션을 불러오면서, 다른 구역으로 이동하는 명령이 있는지 확인 */
         let mut now_address = address;
         loop {
             let insts = self
@@ -37,9 +44,11 @@ impl PE {
                     {}
             }
         }
+
+        /* 블록 생성 및 반환 */
         let block = self
             .blocks
-            .new_block(section, block_start, Some(block_end), None);
+            .generate_block(section, block_start, Some(block_end), None);
         return (block, relation);
     }
 }
