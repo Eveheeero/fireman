930a29f5d83f7029a9a3a2f6c60517aee592f40e
Eveheeero <xhve00000@naver.com>
2022-11-30 15:24:09 +0000

    fire.rs, address.rs 주석 재작성
    + allow(unused_imports) 제거
    + 불필요한 use문 제거

--- fireball/src/core/address.rs
+++ fireball/src/core/address.rs
@@ -2,25 +2,40 @@ use std::sync::Arc;
 
 use crate::core::{Section, Sections};
 
-use goblin::Object;
-
+/// 타겟 프로그램 내부에서 사용되는 주소에 대한 정보를 담고있는 구조체
+///
+/// 해당 구조체는 여러 섹션 정보로부터, 주어진 오프셋이 어느 위치에 있으며, 파일오프셋은 얼마인지 등을 계산해준다.
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub struct Address {
+    /// Address객체가 어느 섹션에 속하는지를 나타내는 섹션 정보
     section: Arc<Section>,
+    /// Address객체의 가상 주소
+    ///
+    /// ### Note
+    /// - 파일 오프셋이 표현하지 못하는 범위를 가상주소가 표현할 수 있기 때문에, 가상주소를 사용한다.
     virtual_offset: u64,
 }
 
 impl Address {
     /// 파일 오프셋을 기반으로 Address 객체를 생성한다.
+    ///
+    /// ### Arguments
+    /// - `sections: &Sections` - 섹션 정보를 담고있는 Sections 객체
+    /// - `offset: u64` - 파일 오프셋
+    ///
+    /// ### Returns
+    /// - `Result<Self, ()>` - 섹션 정보를 찾을 수 없는 경우 에러를 반환한다.
     pub(crate) fn from_file_offset(sections: &Sections, offset: u64) -> Result<Self, ()> {
-        // 오프셋에 해당하는 섹션 찾기
+        /* 오프셋에 해당하는 섹션 찾기 */
         let section = match sections.from_file_offset(offset) {
             Some(section) => section,
             None => return Err(()),
         };
-        // 섹션정보를 기반으로 가상주소 연산
+
+        /* 섹션정보를 기반으로 가상주소 연산 */
         let virtual_offset = offset - section.file_offset + section.virtual_address;
 
+        /* 객체 생성 및 반환 */
         Ok(Self {
             section,
             virtual_offset,
@@ -28,19 +43,38 @@ impl Address {
     }
 
     /// 가상 주소를 기반으로 Address 객체를 생성한다.
+    ///
+    /// ### Arguments
+    /// - `sections: &Sections` - 섹션 정보를 담고있는 Sections 객체
+    /// - `virtual_offset: u64` - 가상 주소
+    ///
+    /// ### Returns
+    /// - `Result<Self, ()>` - 섹션 정보를 찾을 수 없는 경우 에러를 반환한다.
+    ///
+    /// ### Todo
+    /// - 섹션 정보를 찾을 수 없는 경우 에러를 반환하는 것이 아닌, None을 반환하도록 수정해야 한다.
     pub(crate) fn from_virtual_address(sections: &Sections, offset: u64) -> Result<Self, ()> {
-        // 가상주소에 해당하는 섹션 찾기
+        /* 가상주소에 해당하는 섹션 찾기 */
         let section = match sections.from_virtual_address(offset) {
             Some(section) => section,
             None => return Err(()),
         };
 
+        /* 객체 생성 및 값 반환 */
         Ok(Self {
             section,
             virtual_offset: offset,
         })
     }
 
+    /// 파일 오프셋을 반환한다.
+    ///
+    /// ### Returns
+    /// - `u64` - 파일 오프셋
+    ///
+    /// ### Todo
+    /// - 파일 오프셋이 존재하지 않을 수 있으므로 파일 오프셋을 반환하는 것이 아닌, Option<u64>을 반환하도록 수정해야 한다.
+    /// - 다음 섹션에 대한 범위 체크를 진행해야 한다. (기본 바이너리에 없는 가상 주소를 참조하는 경우, None을 반환해야 한다.)
     pub(crate) fn get_file_offset(&self) -> u64 {
         let virtual_offset = self.virtual_offset;
         let section_virtual_offset_start = self.section.virtual_address;
@@ -48,10 +82,18 @@ impl Address {
         (virtual_offset - section_virtual_offset_start) + section_file_offset_start
     }
 
+    /// 가상 주소를 반환한다.
+    ///
+    /// ### Returns
+    /// - `u64` - 가상 주소
     pub(crate) fn get_virtual_address(&self) -> u64 {
         self.virtual_offset
     }
 
+    /// 섹션 정보를 반환한다.
+    ///
+    /// ### Returns
+    /// - `Arc<Section>` - 섹션 정보
     pub(crate) fn get_section(&self) -> Arc<Section> {
         self.section.clone()
     }
--- fireball/src/core/fire.rs
+++ fireball/src/core/fire.rs
@@ -1,14 +1,18 @@
 use std::sync::Arc;
 
-use super::{Address, Block, InstructionHistory, Relation, Sections};
+use super::{Address, Block, InstructionHistory, Sections};
 use crate::prelude::{BlockParsingError, DecompileError, IoError};
 
 /// ## Main Decompile Trait
+/// 해당 Trait는 디컴파일러를 작성할 때, 해당라이브러리에서 필요한 기능들을 정의해준다.
 pub trait Fire {
     /// 파일 경로를 기반으로 파서 객체를 생성한다.
     ///
     /// ### Arguments
-    /// - path - 읽어올 파일 경로
+    /// - `path: &str` - 읽어올 파일 경로
+    ///
+    /// ### Returns
+    /// - `Result<Self, IoError>` - 파일을 읽는것을 성공할 시 파서 객체를 반환하며, 파일을 읽을 수 없을 시 에러를 반환한다.
     fn from_path(path: &str) -> Result<Self, IoError>
     where
         Self: Sized;
@@ -16,7 +20,10 @@ pub trait Fire {
     /// 바이너리를 기반으로 파서 객체를 생성한다.
     ///
     /// ### Arguments
-    /// - binary - 파싱할 바이너리 데이터
+    /// - `binary: Vec<u8>` - 파싱할 바이너리 데이터
+    ///
+    /// ### Returns
+    /// - `Result<Self, IoError>` - 파서 객체를 반환하며, 항상 성공한다.
     fn from_binary(binary: Vec<u8>) -> Result<Self, IoError>
     where
         Self: Sized;
@@ -24,7 +31,7 @@ pub trait Fire {
     /// 파일 경로를 반환한다.
     ///
     /// ### Returns
-    /// - Option\<String\> - 파일 경로
+    /// - `Option<String>` - 파일 경로
     ///
     /// ### Note
     /// - 해당 함수는 아무런 추가적인 연산을 수행하지 않는다.
@@ -33,28 +40,50 @@ pub trait Fire {
     /// 바이너리를 반환한다.
     ///
     /// ### Returns
-    /// - &Vec\<u8\> - 원본 파일 바이너리 데이터
+    /// - `&Vec<u8>` - 원본 파일 바이너리 데이터
     ///
     /// ### Note
     /// - 해당 함수는 아무런 추가적인 연산을 수행하지 않는다.
     fn get_binary(&self) -> &Vec<u8>;
 
-    /// 파일의 모든 내용을 분석한다.
-    fn decom_all(&self) -> Result<(), DecompileError>;
-
-    /// 엔트리포인트부터 분석한다.
-    fn decom_from_entry(&self) -> Result<(), DecompileError>;
-
-    /// 파일 오프셋을 기반으로 분석한다.
-    fn decom_from_file_offset(&self, address: u64) -> Result<(), DecompileError>;
-
-    /// 가상 주소를 기반으로 분석한다.
-    fn decom_from_virtual_address(&self, address: u64) -> Result<(), DecompileError>;
-
-    /// 모든 섹션 정보를 가져온다.
+    /// 파일의 전체 내용을 디컴파일한다.
     ///
     /// ### Returns
-    /// - Arc\<Sections\> - 구조체 내부에 저장되어있는 섹션 정보의 모음집
+    /// - `Result<(), DecompileError>` - 디컴파일에 실패할 시 에러를 반환한다.
+    ///
+    /// ### Todo
+    /// 해당 방법은 난독화에 대비하여, 분석을 마친 후, 이미 분석한 오프셋에서 일정 오프셋을 이동시켜 제대로 된 인스트럭션이 나오는지 확인하는 등으로 검사를 수행할 예정이다.
+    /// 엔트리부터 분석을 시작한 후, 분석이 끝난 오프셋에서 일정 오프셋을 이동시킨 후 decom_from_file_offset등을 이용해 분석?
+    fn decom_all(&self) -> Result<(), DecompileError>;
+
+    /// 엔트리포인트부터 디컴파일을 수행한다.
+    ///
+    /// ### Returns
+    /// - `Result<(), DecompileError>` - 디컴파일에 실패할 시 에러를 반환한다.
+    fn decom_from_entry(&self) -> Result<(), DecompileError>;
+
+    /// 주어진 파일 오프셋부터 디컴파일을 수행한다.
+    ///
+    /// ### Arguments
+    /// - `address: u64` - 분석을 시작할 파일 오프셋
+    ///
+    /// ### Returns
+    /// - `Result<(), DecompileError>` - 디컴파일에 실패할 시 에러를 반환한다.
+    fn decom_from_file_offset(&self, address: u64) -> Result<(), DecompileError>;
+
+    /// 주어진 가상 주소부터 디컴파일을 수행한다.
+    ///
+    /// ### Arguments
+    /// - `address: u64` - 분석을 시작할 파일 오프셋
+    ///
+    /// ### Returns
+    /// - `Result<(), DecompileError>` - 디컴파일에 실패할 시 에러를 반환한다.
+    fn decom_from_virtual_address(&self, address: u64) -> Result<(), DecompileError>;
+
+    /// 분석 후 나온 모든 섹션의 정보를 가져온다.
+    ///
+    /// ### Returns
+    /// - `Arc<Sections>` - 디컴파일러 객체가 가지고 있는 섹션 정보를 관리하는 객체
     ///
     /// ### Note
     /// - 해당 함수는 아무런 추가적인 연산을 수행하지 않는다.
@@ -63,10 +92,11 @@ pub trait Fire {
     /// 파일 오프셋과 이어진 블록 정보를 가져온다.
     ///
     /// ### Arguments
-    /// - adress - 파싱할 블록의 시작 주소
+    /// - `adress: Address` - 파싱할 블록의 시작 주소
+    /// - `history: &mut InstructionHistory` - 분석을 수행하면서 디스어셈블한 인스트럭션의 정보를 저장할 객체
     ///
     /// ### Returns
-    /// - Arc\<Block\> - 파싱된 블록 객체
+    /// - `Result<Arc<Block>, BlockParsingError>` - 블럭 파싱에 성공 시 블럭 객체를 반환하며, 오류가 발생했을 시 에러를 반환한다.
     ///
     /// ### Note
     /// - 해당 함수는 인자로 주어진 주소로부터 어떤 주소까지 점프 없이 수행되는지 파악 후 블록을 생성하여 반환한다.
