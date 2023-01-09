4a1a262a1734fae8398587b7edf1d7107c72b2c1
Eveheeero <xhve00000@naver.com>
2023-01-09 03:53:01 +0000

    core 내부 세 개 파일 주석 재작성
    - blocks.rs
    - instruction_history.rs
    - instruction.rs
    위의 주석 재작성

--- fireball/src/core/blocks.rs
+++ fireball/src/core/blocks.rs
@@ -2,21 +2,35 @@ use std::sync::Arc;
 
 use super::{Address, Block, Section};
 
-/// 블록들을 가지고 있는 구조체
+/// 어셈블리 단위의 블럭들을 고나리하는 구조체
+///
+/// 해당 구조체를 이용해 블럭을 생성하고, 이미 존재하는 블럭을 가져온다.
 pub struct Blocks {
+    /// 블럭들의 실제 데이터
     data: std::sync::RwLock<std::collections::HashSet<Arc<Block>>>,
 }
 
 impl Blocks {
-    /// 블록들을 가지고 있는 구조체를 생성한다.
+    /// 블럭 저장소 구조체를 생성한다.
+    ///
+    /// ### Returns
+    /// - `Arc<Self>`: 생성된 블럭 저장소 구조체
     pub(crate) fn new() -> Arc<Self> {
         Arc::new(Self {
             data: Default::default(),
         })
     }
 
-    /// 새 블록을 추가한다.
-    /// 새로 만들어진 블록은 해당 구조체 내부에 저장되며, 복사본을 반환한다.
+    /// 저장소에 새 블럭을 생성한다.
+    ///
+    /// ### Arguments
+    /// - `section: Arc<Section>`: 블럭이 속한 섹션
+    /// - `start_address_virtual: Address`: 블럭의 시작 주소
+    /// - `end_address_virtual: Option<Address>`: 블럭의 끝 주소
+    /// - `name: Option<String>`: 블럭의 이름
+    ///
+    /// ### Returns
+    /// - `Arc<Block>`: 생성된 블럭
     pub(crate) fn generate_block(
         &self,
         section: Arc<Section>,
@@ -43,7 +57,13 @@ impl Blocks {
         new_block
     }
 
-    /// 시작지점이 N인 블록 객체를 반환한다.
+    /// 주어진 주소를 시작주소로 가진 블럭을 반환한다.
+    ///
+    /// ### Arguments
+    /// - `address: Address`: 대상 주소
+    ///
+    /// ### Returns
+    /// - `Option<Arc<Block>>`: 검출된 블럭
     pub(crate) fn find_from_start_address(&self, address: Address) -> Option<Arc<Block>> {
         /* 저장소의 락 해제 */
         let blocks_reader = &self.data.read().unwrap();
