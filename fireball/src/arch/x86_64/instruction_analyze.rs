e3d2733f7085f5cde05196c79fcaa696c7914234
Eveheeero <xhve00000@naver.com>
2023-06-06 15:29:43 +0000

    x86_64 인스트럭션 변환 모듈 초안 작성

---
+++ fireball/src/arch/x86_64/instruction_analyze.rs
@@ -0,0 +1,15 @@
+//! x86_64 아키텍처 인스트럭션을 IR로 변환하는 함수가 담긴 모듈
+
+use crate::core::Instruction;
+
+/// 어셈블리 인스트럭션을 받아 IR 명령으로 변환한다.
+///
+/// ### Arguments
+/// - `instruction` : 어셈블리 인스트럭션
+///
+/// ### Returns
+/// `TEMP` : IR 명령 배열
+#[allow(unused)]
+fn create_ir_statement(instruction: &Instruction) -> &'static [Box<dyn std::any::Any>] {
+    todo!()
+}
---
+++ fireball/src/arch/x86_64/mod.rs
@@ -0,0 +1,3 @@
+//! x86_64 아키텍처 어셈블리를 IR로 변환하는 모듈
+
+mod instruction_analyze;
