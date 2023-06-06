63095fb32fb1c6487adb89eb5eaae953197d0168
Eveheeero <xhve00000@naver.com>
2023-06-06 14:56:15 +0000

    X64Mut 작성

--- fireball/src/ir/x86_64.rs
+++ fireball/src/ir/x86_64.rs
@@ -1,6 +1,7 @@
 //! x86_64 CPU 컴퓨터를 IR구조로 변환하는데 사용되는 서브모듈입니다.
 
 mod base;
+mod r#mut;
 
 use crate::prelude::BitSlice;
 
@@ -217,4 +218,219 @@ pub trait X64 {
     fn tmp16(&self) -> &BitSlice;
     fn tmp32(&self) -> &BitSlice;
     fn tmp64(&self) -> &BitSlice;
+}
+
+/// X64(32비트 포함) 수정할 수 있는 레지스터를 가져오는 인터페이스입니다.
+pub trait X64Mut {
+    fn const_bitslice_to_mut(data: &BitSlice) -> &mut BitSlice;
+
+    fn rax(&mut self) -> &mut BitSlice;
+    fn eax(&mut self) -> &mut BitSlice;
+    fn ax(&mut self) -> &mut BitSlice;
+    fn al(&mut self) -> &mut BitSlice;
+    fn ah(&mut self) -> &mut BitSlice;
+
+    fn rbx(&mut self) -> &mut BitSlice;
+    fn ebx(&mut self) -> &mut BitSlice;
+    fn bx(&mut self) -> &mut BitSlice;
+    fn bl(&mut self) -> &mut BitSlice;
+    fn bh(&mut self) -> &mut BitSlice;
+
+    fn rcx(&mut self) -> &mut BitSlice;
+    fn ecx(&mut self) -> &mut BitSlice;
+    fn cx(&mut self) -> &mut BitSlice;
+    fn cl(&mut self) -> &mut BitSlice;
+    fn ch(&mut self) -> &mut BitSlice;
+
+    fn rdx(&mut self) -> &mut BitSlice;
+    fn edx(&mut self) -> &mut BitSlice;
+    fn dx(&mut self) -> &mut BitSlice;
+    fn dl(&mut self) -> &mut BitSlice;
+    fn dh(&mut self) -> &mut BitSlice;
+
+    fn rsp(&mut self) -> &mut BitSlice;
+    fn esp(&mut self) -> &mut BitSlice;
+    fn sp(&mut self) -> &mut BitSlice;
+    fn spl(&mut self) -> &mut BitSlice;
+
+    fn rbp(&mut self) -> &mut BitSlice;
+    fn ebp(&mut self) -> &mut BitSlice;
+    fn bp(&mut self) -> &mut BitSlice;
+    fn bpl(&mut self) -> &mut BitSlice;
+
+    fn rsi(&mut self) -> &mut BitSlice;
+    fn esi(&mut self) -> &mut BitSlice;
+    fn si(&mut self) -> &mut BitSlice;
+    fn sil(&mut self) -> &mut BitSlice;
+
+    fn rdi(&mut self) -> &mut BitSlice;
+    fn edi(&mut self) -> &mut BitSlice;
+    fn di(&mut self) -> &mut BitSlice;
+    fn dil(&mut self) -> &mut BitSlice;
+
+    fn r8(&mut self) -> &mut BitSlice;
+    fn r8d(&mut self) -> &mut BitSlice;
+    fn r8w(&mut self) -> &mut BitSlice;
+    fn r8b(&mut self) -> &mut BitSlice;
+
+    fn r9(&mut self) -> &mut BitSlice;
+    fn r9d(&mut self) -> &mut BitSlice;
+    fn r9w(&mut self) -> &mut BitSlice;
+    fn r9b(&mut self) -> &mut BitSlice;
+
+    fn r10(&mut self) -> &mut BitSlice;
+    fn r10d(&mut self) -> &mut BitSlice;
+    fn r10w(&mut self) -> &mut BitSlice;
+    fn r10b(&mut self) -> &mut BitSlice;
+
+    fn r11(&mut self) -> &mut BitSlice;
+    fn r11d(&mut self) -> &mut BitSlice;
+    fn r11w(&mut self) -> &mut BitSlice;
+    fn r11b(&mut self) -> &mut BitSlice;
+
+    fn r12(&mut self) -> &mut BitSlice;
+    fn r12d(&mut self) -> &mut BitSlice;
+    fn r12w(&mut self) -> &mut BitSlice;
+    fn r12b(&mut self) -> &mut BitSlice;
+
+    fn r13(&mut self) -> &mut BitSlice;
+    fn r13d(&mut self) -> &mut BitSlice;
+    fn r13w(&mut self) -> &mut BitSlice;
+    fn r13b(&mut self) -> &mut BitSlice;
+
+    fn r14(&mut self) -> &mut BitSlice;
+    fn r14d(&mut self) -> &mut BitSlice;
+    fn r14w(&mut self) -> &mut BitSlice;
+    fn r14b(&mut self) -> &mut BitSlice;
+
+    fn r15(&mut self) -> &mut BitSlice;
+    fn r15d(&mut self) -> &mut BitSlice;
+    fn r15w(&mut self) -> &mut BitSlice;
+    fn r15b(&mut self) -> &mut BitSlice;
+
+    fn cs(&mut self) -> &mut BitSlice;
+    fn ds(&mut self) -> &mut BitSlice;
+    fn es(&mut self) -> &mut BitSlice;
+    fn fs(&mut self) -> &mut BitSlice;
+    fn gs(&mut self) -> &mut BitSlice;
+    fn ss(&mut self) -> &mut BitSlice;
+
+    fn rip(&mut self) -> &mut BitSlice;
+    fn eip(&mut self) -> &mut BitSlice;
+    fn ip(&mut self) -> &mut BitSlice;
+
+    fn rflags(&mut self) -> &mut BitSlice;
+    fn eflags(&mut self) -> &mut BitSlice;
+    fn flags(&mut self) -> &mut BitSlice;
+    fn cf(&mut self) -> &mut BitSlice;
+    fn pf(&mut self) -> &mut BitSlice;
+    fn af(&mut self) -> &mut BitSlice;
+    fn zf(&mut self) -> &mut BitSlice;
+    fn sf(&mut self) -> &mut BitSlice;
+    fn tf(&mut self) -> &mut BitSlice;
+    fn r#if(&mut self) -> &mut BitSlice;
+    fn df(&mut self) -> &mut BitSlice;
+    fn of(&mut self) -> &mut BitSlice;
+    fn iopl(&mut self) -> &mut BitSlice;
+    fn nt(&mut self) -> &mut BitSlice;
+    fn rf(&mut self) -> &mut BitSlice;
+    fn vm(&mut self) -> &mut BitSlice;
+    fn ac(&mut self) -> &mut BitSlice;
+    fn vif(&mut self) -> &mut BitSlice;
+    fn vip(&mut self) -> &mut BitSlice;
+    fn id(&mut self) -> &mut BitSlice;
+
+    fn less(&mut self) -> &mut BitSlice;
+    fn less_or_equal(&mut self) -> &mut BitSlice;
+    fn below_or_equal(&mut self) -> &mut BitSlice;
+
+    fn fpu_status_word(&mut self) -> &mut BitSlice;
+    fn fpu_ie(&mut self) -> &mut BitSlice;
+    fn fpu_de(&mut self) -> &mut BitSlice;
+    fn fpu_ze(&mut self) -> &mut BitSlice;
+    fn fpu_oe(&mut self) -> &mut BitSlice;
+    fn fpu_ue(&mut self) -> &mut BitSlice;
+    fn fpu_pe(&mut self) -> &mut BitSlice;
+    fn fpu_sf(&mut self) -> &mut BitSlice;
+    fn fpu_es(&mut self) -> &mut BitSlice;
+    fn fpu_c0(&mut self) -> &mut BitSlice;
+    fn fpu_c1(&mut self) -> &mut BitSlice;
+    fn fpu_c2(&mut self) -> &mut BitSlice;
+    fn fpu_top(&mut self) -> &mut BitSlice;
+    fn fpu_c3(&mut self) -> &mut BitSlice;
+    fn fpu_b(&mut self) -> &mut BitSlice;
+
+    fn st0(&mut self) -> &mut BitSlice;
+    fn st1(&mut self) -> &mut BitSlice;
+    fn st2(&mut self) -> &mut BitSlice;
+    fn st3(&mut self) -> &mut BitSlice;
+    fn st4(&mut self) -> &mut BitSlice;
+    fn st5(&mut self) -> &mut BitSlice;
+    fn st6(&mut self) -> &mut BitSlice;
+    fn st7(&mut self) -> &mut BitSlice;
+
+    fn mm0(&mut self) -> &mut BitSlice;
+    fn mm1(&mut self) -> &mut BitSlice;
+    fn mm2(&mut self) -> &mut BitSlice;
+    fn mm3(&mut self) -> &mut BitSlice;
+    fn mm4(&mut self) -> &mut BitSlice;
+    fn mm5(&mut self) -> &mut BitSlice;
+    fn mm6(&mut self) -> &mut BitSlice;
+    fn mm7(&mut self) -> &mut BitSlice;
+
+    fn xmm0(&mut self) -> &mut BitSlice;
+    fn xmm1(&mut self) -> &mut BitSlice;
+    fn xmm2(&mut self) -> &mut BitSlice;
+    fn xmm3(&mut self) -> &mut BitSlice;
+    fn xmm4(&mut self) -> &mut BitSlice;
+    fn xmm5(&mut self) -> &mut BitSlice;
+    fn xmm6(&mut self) -> &mut BitSlice;
+    fn xmm7(&mut self) -> &mut BitSlice;
+    fn xmm8(&mut self) -> &mut BitSlice;
+    fn xmm9(&mut self) -> &mut BitSlice;
+    fn xmm10(&mut self) -> &mut BitSlice;
+    fn xmm11(&mut self) -> &mut BitSlice;
+    fn xmm12(&mut self) -> &mut BitSlice;
+    fn xmm13(&mut self) -> &mut BitSlice;
+    fn xmm14(&mut self) -> &mut BitSlice;
+    fn xmm15(&mut self) -> &mut BitSlice;
+
+    fn cr0(&mut self) -> &mut BitSlice;
+    fn cr1(&mut self) -> &mut BitSlice;
+    fn cr2(&mut self) -> &mut BitSlice;
+    fn cr3(&mut self) -> &mut BitSlice;
+    fn cr4(&mut self) -> &mut BitSlice;
+    fn cr5(&mut self) -> &mut BitSlice;
+    fn cr6(&mut self) -> &mut BitSlice;
+    fn cr7(&mut self) -> &mut BitSlice;
+    fn cr8(&mut self) -> &mut BitSlice;
+    fn cr9(&mut self) -> &mut BitSlice;
+    fn cr10(&mut self) -> &mut BitSlice;
+    fn cr11(&mut self) -> &mut BitSlice;
+    fn cr12(&mut self) -> &mut BitSlice;
+    fn cr13(&mut self) -> &mut BitSlice;
+    fn cr14(&mut self) -> &mut BitSlice;
+    fn cr15(&mut self) -> &mut BitSlice;
+
+    fn dr0(&mut self) -> &mut BitSlice;
+    fn dr1(&mut self) -> &mut BitSlice;
+    fn dr2(&mut self) -> &mut BitSlice;
+    fn dr3(&mut self) -> &mut BitSlice;
+    fn dr4(&mut self) -> &mut BitSlice;
+    fn dr5(&mut self) -> &mut BitSlice;
+    fn dr6(&mut self) -> &mut BitSlice;
+    fn dr7(&mut self) -> &mut BitSlice;
+    fn dr8(&mut self) -> &mut BitSlice;
+    fn dr9(&mut self) -> &mut BitSlice;
+    fn dr10(&mut self) -> &mut BitSlice;
+    fn dr11(&mut self) -> &mut BitSlice;
+    fn dr12(&mut self) -> &mut BitSlice;
+    fn dr13(&mut self) -> &mut BitSlice;
+    fn dr14(&mut self) -> &mut BitSlice;
+    fn dr15(&mut self) -> &mut BitSlice;
+
+    fn tmp8(&mut self) -> &mut BitSlice;
+    fn tmp16(&mut self) -> &mut BitSlice;
+    fn tmp32(&mut self) -> &mut BitSlice;
+    fn tmp64(&mut self) -> &mut BitSlice;
 }
---
+++ fireball/src/ir/x86_64/mut.rs
@@ -0,0 +1,239 @@
+use crate::{
+    ir::{
+        x86_64::{X64Mut, X64},
+        Ir,
+    },
+    prelude::BitSlice,
+};
+
+/// x64_to_mut!(rax);가 들어올 경우 다음을 반환함
+/// ```rust
+/// fn rax(&mut self) -> &mut BitSlice {
+///     Self::const_bitslice_to_mut(X64::rax(self))
+/// }
+/// ```
+macro_rules! x64_to_mut {
+    ($reg:ident) => {
+        #[inline(always)]
+        fn $reg(&mut self) -> &mut BitSlice {
+            Self::const_bitslice_to_mut(X64::$reg(self))
+        }
+    };
+}
+
+impl X64Mut for Ir {
+    #[inline(always)]
+    fn const_bitslice_to_mut(data: &BitSlice) -> &mut BitSlice {
+        unsafe { &mut *(data as *const BitSlice as *mut BitSlice) }
+    }
+
+    x64_to_mut!(rax);
+    x64_to_mut!(eax);
+    x64_to_mut!(ax);
+    x64_to_mut!(al);
+    x64_to_mut!(ah);
+
+    x64_to_mut!(rbx);
+    x64_to_mut!(ebx);
+    x64_to_mut!(bx);
+    x64_to_mut!(bl);
+    x64_to_mut!(bh);
+
+    x64_to_mut!(rcx);
+    x64_to_mut!(ecx);
+    x64_to_mut!(cx);
+    x64_to_mut!(cl);
+    x64_to_mut!(ch);
+
+    x64_to_mut!(rdx);
+    x64_to_mut!(edx);
+    x64_to_mut!(dx);
+    x64_to_mut!(dl);
+    x64_to_mut!(dh);
+
+    x64_to_mut!(rsp);
+    x64_to_mut!(esp);
+    x64_to_mut!(sp);
+    x64_to_mut!(spl);
+
+    x64_to_mut!(rbp);
+    x64_to_mut!(ebp);
+    x64_to_mut!(bp);
+    x64_to_mut!(bpl);
+
+    x64_to_mut!(rsi);
+    x64_to_mut!(esi);
+    x64_to_mut!(si);
+    x64_to_mut!(sil);
+
+    x64_to_mut!(rdi);
+    x64_to_mut!(edi);
+    x64_to_mut!(di);
+    x64_to_mut!(dil);
+
+    x64_to_mut!(r8);
+    x64_to_mut!(r8d);
+    x64_to_mut!(r8w);
+    x64_to_mut!(r8b);
+
+    x64_to_mut!(r9);
+    x64_to_mut!(r9d);
+    x64_to_mut!(r9w);
+    x64_to_mut!(r9b);
+
+    x64_to_mut!(r10);
+    x64_to_mut!(r10d);
+    x64_to_mut!(r10w);
+    x64_to_mut!(r10b);
+
+    x64_to_mut!(r11);
+    x64_to_mut!(r11d);
+    x64_to_mut!(r11w);
+    x64_to_mut!(r11b);
+
+    x64_to_mut!(r12);
+    x64_to_mut!(r12d);
+    x64_to_mut!(r12w);
+    x64_to_mut!(r12b);
+
+    x64_to_mut!(r13);
+    x64_to_mut!(r13d);
+    x64_to_mut!(r13w);
+    x64_to_mut!(r13b);
+
+    x64_to_mut!(r14);
+    x64_to_mut!(r14d);
+    x64_to_mut!(r14w);
+    x64_to_mut!(r14b);
+
+    x64_to_mut!(r15);
+    x64_to_mut!(r15d);
+    x64_to_mut!(r15w);
+    x64_to_mut!(r15b);
+
+    x64_to_mut!(cs);
+    x64_to_mut!(ds);
+    x64_to_mut!(es);
+    x64_to_mut!(fs);
+    x64_to_mut!(gs);
+    x64_to_mut!(ss);
+
+    x64_to_mut!(rip);
+    x64_to_mut!(eip);
+    x64_to_mut!(ip);
+
+    x64_to_mut!(rflags);
+    x64_to_mut!(eflags);
+    x64_to_mut!(flags);
+    x64_to_mut!(cf);
+    x64_to_mut!(pf);
+    x64_to_mut!(af);
+    x64_to_mut!(zf);
+    x64_to_mut!(sf);
+    x64_to_mut!(tf);
+    x64_to_mut!(r#if);
+    x64_to_mut!(df);
+    x64_to_mut!(of);
+    x64_to_mut!(iopl);
+    x64_to_mut!(nt);
+    x64_to_mut!(rf);
+    x64_to_mut!(vm);
+    x64_to_mut!(ac);
+    x64_to_mut!(vif);
+    x64_to_mut!(vip);
+    x64_to_mut!(id);
+
+    x64_to_mut!(less);
+    x64_to_mut!(less_or_equal);
+    x64_to_mut!(below_or_equal);
+
+    x64_to_mut!(fpu_status_word);
+    x64_to_mut!(fpu_ie);
+    x64_to_mut!(fpu_de);
+    x64_to_mut!(fpu_ze);
+    x64_to_mut!(fpu_oe);
+    x64_to_mut!(fpu_ue);
+    x64_to_mut!(fpu_pe);
+    x64_to_mut!(fpu_sf);
+    x64_to_mut!(fpu_es);
+    x64_to_mut!(fpu_c0);
+    x64_to_mut!(fpu_c1);
+    x64_to_mut!(fpu_c2);
+    x64_to_mut!(fpu_top);
+    x64_to_mut!(fpu_c3);
+    x64_to_mut!(fpu_b);
+
+    x64_to_mut!(st0);
+    x64_to_mut!(st1);
+    x64_to_mut!(st2);
+    x64_to_mut!(st3);
+    x64_to_mut!(st4);
+    x64_to_mut!(st5);
+    x64_to_mut!(st6);
+    x64_to_mut!(st7);
+
+    x64_to_mut!(mm0);
+    x64_to_mut!(mm1);
+    x64_to_mut!(mm2);
+    x64_to_mut!(mm3);
+    x64_to_mut!(mm4);
+    x64_to_mut!(mm5);
+    x64_to_mut!(mm6);
+    x64_to_mut!(mm7);
+
+    x64_to_mut!(xmm0);
+    x64_to_mut!(xmm1);
+    x64_to_mut!(xmm2);
+    x64_to_mut!(xmm3);
+    x64_to_mut!(xmm4);
+    x64_to_mut!(xmm5);
+    x64_to_mut!(xmm6);
+    x64_to_mut!(xmm7);
+    x64_to_mut!(xmm8);
+    x64_to_mut!(xmm9);
+    x64_to_mut!(xmm10);
+    x64_to_mut!(xmm11);
+    x64_to_mut!(xmm12);
+    x64_to_mut!(xmm13);
+    x64_to_mut!(xmm14);
+    x64_to_mut!(xmm15);
+
+    x64_to_mut!(cr0);
+    x64_to_mut!(cr1);
+    x64_to_mut!(cr2);
+    x64_to_mut!(cr3);
+    x64_to_mut!(cr4);
+    x64_to_mut!(cr5);
+    x64_to_mut!(cr6);
+    x64_to_mut!(cr7);
+    x64_to_mut!(cr8);
+    x64_to_mut!(cr9);
+    x64_to_mut!(cr10);
+    x64_to_mut!(cr11);
+    x64_to_mut!(cr12);
+    x64_to_mut!(cr13);
+    x64_to_mut!(cr14);
+    x64_to_mut!(cr15);
+
+    x64_to_mut!(dr0);
+    x64_to_mut!(dr1);
+    x64_to_mut!(dr2);
+    x64_to_mut!(dr3);
+    x64_to_mut!(dr4);
+    x64_to_mut!(dr5);
+    x64_to_mut!(dr6);
+    x64_to_mut!(dr7);
+    x64_to_mut!(dr8);
+    x64_to_mut!(dr9);
+    x64_to_mut!(dr10);
+    x64_to_mut!(dr11);
+    x64_to_mut!(dr12);
+    x64_to_mut!(dr13);
+    x64_to_mut!(dr14);
+    x64_to_mut!(dr15);
+
+    x64_to_mut!(tmp8);
+    x64_to_mut!(tmp16);
+    x64_to_mut!(tmp32);
+    x64_to_mut!(tmp64);
+}
