9ff564aa659d9be87939b1f7ff7da625bf0dc9be
Eveheeero <xhve00000@naver.com>
2025-05-02 14:57:15 +0900

    Ir shortcut macros 생성 및 extend_undefined_registers 생성

---
+++ fireball/src/arch/x86_64/instruction_analyze_shortcuts/macros.rs
@@ -0,0 +1,20 @@
+use super::*;
+
+/// extend result to undefined flags
+/// ```rust,ignore
+/// extend_undefined_flags([add, assignment], size, &[&of, &sf, &zf, &af, &cf, &pf])
+/// ```
+#[inline]
+#[must_use]
+pub(in crate::arch) fn extend_undefined_flags(
+    ir: &[IrStatement],
+    affected_registers: &[&crate::ir::Register],
+) -> Box<[IrStatement]> {
+    let mut result: Vec<_> = ir.into();
+    for register in affected_registers {
+        let register = r(&register);
+        let assignment = assign(undefined_data(), register.clone(), size_relative(register));
+        result.push(assignment);
+    }
+    result.into_boxed_slice()
+}
