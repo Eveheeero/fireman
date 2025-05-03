d7e37977329b55a0b8f21d4e29c6aaedf8113bed
Eveheeero <xhve00000@naver.com>
2025-05-03 11:31:48 +0900

    DataAccess 분석 로직 작성

--- fireball/src/ir/analyze/data_access.rs
+++ fireball/src/ir/analyze/data_access.rs
@@ -1,4 +1,8 @@
-use crate::ir::{data::DataAccess, statements::IrStatement, Ir};
+use crate::ir::{
+    data::{AccessSize, DataAccess, DataAccessType},
+    statements::{IrStatement, IrStatementSpecial},
+    Ir,
+};
 
 pub fn analyze_data_access(ir: &Ir) -> Vec<DataAccess> {
     if ir.statements.is_right() {
@@ -12,5 +16,67 @@ pub fn analyze_data_access(ir: &Ir) -> Vec<DataAccess> {
 }
 
 pub fn analyze_data_access_raw(v: &mut Vec<DataAccess>, statement: &IrStatement) {
-    todo!()
+    match statement {
+        IrStatement::Assignment { from, to, size } => {
+            v.push(DataAccess::new(
+                from.clone(),
+                DataAccessType::Read,
+                size.clone(),
+            ));
+            v.push(DataAccess::new(
+                to.clone(),
+                DataAccessType::Write,
+                size.clone(),
+            ));
+            match size {
+                AccessSize::ResultOfBit(aos) | AccessSize::ResultOfByte(aos) => {
+                    v.push(DataAccess::new(
+                        aos.clone(),
+                        DataAccessType::Read,
+                        AccessSize::Unlimited,
+                    ));
+                }
+                AccessSize::RelativeWith(_)
+                | AccessSize::ArchitectureSize
+                | AccessSize::Unlimited => {}
+            }
+        }
+        IrStatement::Jump { target } | IrStatement::Call { target } => {
+            v.push(DataAccess::new(
+                target.clone(),
+                DataAccessType::Read,
+                AccessSize::ArchitectureSize,
+            ));
+        }
+        IrStatement::Condition {
+            condition,
+            true_branch,
+            false_branch,
+        } => {
+            v.push(DataAccess::new(
+                condition.clone(),
+                DataAccessType::Read,
+                AccessSize::Unlimited,
+            ));
+            for statement in true_branch.iter().chain(false_branch.iter()) {
+                analyze_data_access_raw(v, statement);
+            }
+        }
+        IrStatement::Special(IrStatementSpecial::ArchitectureByteSizeCondition {
+            condition: _,
+            true_branch,
+            false_branch,
+        }) => {
+            for statement in true_branch.iter().chain(false_branch.iter()) {
+                analyze_data_access_raw(v, statement);
+            }
+        }
+
+        IrStatement::Undefined
+        | IrStatement::Exception(_)
+        | IrStatement::Halt
+        | IrStatement::Special(IrStatementSpecial::Assertion { .. })
+        | IrStatement::Special(IrStatementSpecial::TypeSpecified { .. })
+        | IrStatement::Special(IrStatementSpecial::CalcFlagsAutomatically { .. }) => {}
+    }
 }
