ef8d5386f8c5a7b03df8794a4d3bad379a02fac8
Eveheeero <xhve00000@naver.com>
2025-05-06 15:52:05 +0900

    Ir Variable 분석 관련 구조체 추가 작성 및 분석 로직 추가 작성

--- fireball/src/ir/analyze/variables.rs
+++ fireball/src/ir/analyze/variables.rs
@@ -1,19 +1,66 @@
 use crate::{
     core::Address,
-    ir::{data::IrData, statements::IrStatement, Ir, IrBlock},
+    ir::{
+        analyze::DataType,
+        data::{AccessSize, DataAccess, IrData, IrDataContainable},
+        statements::IrStatement,
+        Ir, IrBlock,
+    },
     utils::Aos,
 };
+pub use private::IrVariable;
 use std::collections::HashSet;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct IrVariable {
-    live_in: Address,
-    live_out: Address,
-    location: Aos<IrData>,
+mod private {
+    use super::*;
+    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+    pub struct IrVariable {
+        /// Index of Ir in IrBlock
+        pub live_in: usize,
+        /// Index of Ir in IrBlock
+        pub shown_in: Vec<usize>,
+        /// Index of Ir in IrBlock
+        pub live_out: Option<usize>,
+        /// Index of Ir in IrBlock
+        accesses: Vec<Option<Vec<DataAccess>>>,
+        pub data_type: DataType,
+    }
+    impl IrVariable {
+        #[inline]
+        pub fn new(live_in_ir_index: usize, data_type: DataType) -> Self {
+            Self {
+                live_in: live_in_ir_index,
+                shown_in: [live_in_ir_index].into(),
+                live_out: None,
+                accesses: Vec::new(),
+                data_type,
+            }
+        }
+        #[inline]
+        pub fn get_data_accesses(&self, ir_index: usize) -> &[DataAccess] {
+            self.accesses
+                .get(ir_index)
+                .unwrap_or(&None)
+                .as_ref()
+                .map(Vec::as_slice)
+                .unwrap_or(&[])
+        }
+        #[inline]
+        pub fn add_data_access(&mut self, ir_index: usize, access: DataAccess) {
+            if self.accesses.len() <= ir_index {
+                self.accesses.resize_with(ir_index + 1, || None);
+            }
+            if self.accesses[ir_index].is_none() {
+                self.accesses[ir_index] = Some(Vec::new());
+            }
+            self.accesses[ir_index].as_mut().unwrap().push(access);
+        }
+    }
 }
 
 pub fn analyze_variables(ir_block: &IrBlock) -> Result<HashSet<IrVariable>, &'static str> {
     let mut result = HashSet::new();
+    let mut living_variables = Vec::new();
     let irs = ir_block.ir.as_ref();
     let known_datatypes_per_ir = ir_block
         .known_datatypes_per_ir
@@ -24,7 +71,31 @@ pub fn analyze_variables(ir_block: &IrBlock) -> Result<HashSet<IrVariable>, &'st
         .as_ref()
         .ok_or_else(|| "Data Access Not Analyzed")?;
 
-    for (ir_index, ir) in irs.iter().enumerate() {}
+    for (ir_index, ir) in irs.iter().enumerate() {
+        if ir.statements.is_right() {
+            continue;
+        }
+        /* 기본 변수 설정 */
+        let statements = ir.statements.as_ref().unwrap_left();
+        let known_datatypes = known_datatypes_per_ir.get(ir_index).unwrap();
+        let data_access = data_access_per_ir.get(ir_index).unwrap();
+        let related_data_per_statement: Vec<Vec<&Aos<IrData>>> = statements
+            .iter()
+            .map(|statement| {
+                let mut related_data = Vec::new();
+                statement.get_related_ir_data(&mut related_data);
+                related_data
+            })
+            .collect();
 
+        for (statement_index, (statement, related_data)) in statements
+            .iter()
+            .zip(related_data_per_statement.iter())
+            .enumerate()
+        {}
+    }
+
+    // 분석이 끝날때까지 살아있는 변수 추가
+    result.extend(living_variables);
     Ok(result)
 }
