e898367fe069bd426918a90df28e8e976fda46d4
Eveheeero <xhve00000@naver.com>
2025-05-11 02:48:25 +0900

    tauri 1 -> 2 버저닝 업

--- firebat/src-tauri/src/main.rs
+++ firebat/src-tauri/src/main.rs
@@ -1,13 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    firebat_lib::run();
 }
--- firebat/src/App.tsx
+++ firebat/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
