63b10dc343422658e56c6ddb74e68ca0866c3b00
Eveheeero <xhve00000@naver.com>
2025-05-11 21:12:13 +0900

    Tauri 기초 템플릿 작성

---
+++ firebat/src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+
+fn main() {
+    firebat_lib::run();
+}
---
+++ firebat/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import logoTransparent from "./assets/logo transparent.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1>Welcome to Tauri + React</h1>
+
+      <div className="row">
+        <img src="/logo colored 512.png" className="logo" alt="logo1" />
+        <img src={logoTransparent} className="logo" alt="transparent logo" />
+      </div>
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
