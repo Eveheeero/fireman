66161020a569c391fa7a87bff91d5b27f87306d4
Eveheeero <xhve00000@naver.com>
2025-05-13 05:08:19 +0900

    Impl firebat's decom_from_address and ir_inspect function =
    - Automate generating result type binding ts file
    - Expose some fireball's api

--- firebat/src-tauri/build.rs
+++ firebat/src-tauri/build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+    let dir_contents = std::fs::read_dir("../../bindings").unwrap();
+    let mut result = String::new();
+    for entry in dir_contents {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let content = std::fs::read_to_string(&path).unwrap();
+        result.push_str(&content);
+        result.push_str("\n");
+    }
+    std::fs::write("../../firebat/src/bindings.ts", result).unwrap();
 }
--- firebat/src-tauri/src/lib.rs
+++ firebat/src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
-use fireball::{Fire, Fireball};
+use fireball::{core::Address, Fire, Fireball};
+use serde::Serialize;
 use std::sync::{Arc, LazyLock, Mutex};
+use ts_bind::TsBind;
 
 struct Firebat {
     path: Option<String>,
@@ -7,15 +9,32 @@ struct Firebat {
 }
 unsafe impl Send for Firebat {}
 impl Firebat {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             path: None,
             fireball: None,
         }
     }
+    fn fireball(&self) -> Result<&Fireball, String> {
+        if let Some(fireball) = &self.fireball {
+            Ok(fireball)
+        } else {
+            Err("Fireball is None".to_string())
+        }
+    }
 }
 
 static APP: LazyLock<Arc<Mutex<Firebat>>> = LazyLock::new(|| Arc::new(Mutex::new(Firebat::new())));
+
+fn parse_address(address: &str) -> Result<u64, String> {
+    if let Ok(address) = address.parse::<u64>() {
+        Ok(address)
+    } else if let Ok(address) = u64::from_str_radix(address, 16) {
+        Ok(address)
+    } else {
+        return Err("Invalid Address".to_string());
+    }
+}
 
 #[tauri::command]
 fn open_file(path: &str) -> Result<(), String> {
@@ -32,9 +51,7 @@ fn open_file(path: &str) -> Result<(), String> {
 #[tauri::command]
 fn decom_from_entry() -> Result<Vec<u64>, String> {
     let app = APP.lock().unwrap();
-    let Some(fireball) = app.fireball.as_ref() else {
-        return Err("fireball is None".to_string());
-    };
+    let fireball = app.fireball()?;
     let result = fireball.decom_from_entry();
     if let Err(e) = result {
         return Err(e.to_string());
@@ -43,25 +60,97 @@ fn decom_from_entry() -> Result<Vec<u64>, String> {
     let reader = result.get_connected_to();
     let mut connected_to = Vec::new();
     for i in reader.iter() {
-        if i.to().is_none() {
+        let Some(to) = i.to() else {
             continue;
-        }
-        let to = &i.to().unwrap();
+        };
         connected_to.push(to.get_virtual_address());
     }
     Ok(connected_to)
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn decom_from_address(address: &str) -> Result<Vec<u64>, String> {
+    let app = APP.lock().unwrap();
+    let fireball = app.fireball()?;
+    let address = parse_address(address)?;
+    let result = fireball.decom_from_virtual_address(address);
+    if let Err(e) = result {
+        return Err(e.to_string());
+    }
+    let result = result.unwrap();
+    let reader = result.get_connected_to();
+    let mut connected_to = Vec::new();
+    for i in reader.iter() {
+        let Some(to) = i.to() else {
+            continue;
+        };
+        connected_to.push(to.get_virtual_address());
+    }
+    Ok(connected_to)
+}
+
+#[derive(Serialize, TsBind)]
+struct IrInspectResult {
+    instruction: String,
+    statements: Option<Vec<String>>,
+    data_accesses: Option<Vec<String>>,
+    data_access_per_ir: Option<Vec<String>>,
+}
+#[tauri::command]
+fn ir_inspect(address: &str) -> Result<Vec<IrInspectResult>, String> {
+    let app = APP.lock().unwrap();
+    let fireball = app.fireball()?;
+    let address = parse_address(address)?;
+    let sections = fireball.get_sections();
+    let address = Address::from_virtual_address(&sections, address);
+    let block = fireball.get_blocks().get_by_containing_address(&address);
+    let block = &block.get(0).ok_or("Block Not Analyzed")?;
+    let ir_block = block.get_ir();
+    let ir_block = ir_block.as_ref().ok_or("Block Not Analyzed")?;
+    let data_access_per_ir = ir_block
+        .data_access_per_ir
+        .as_ref()
+        .ok_or("Block Not Analyzed")?;
+    let known_datatypes_per_ir = ir_block
+        .known_datatypes_per_ir
+        .as_ref()
+        .ok_or("Block Not Analyzed")?;
+    let mut result = Vec::new();
+    for (i, ir) in ir_block.ir().iter().enumerate() {
+        let instruction = ir.instruction.as_ref();
+        if let Some(statements) = ir.statements {
+            let data_access = data_access_per_ir.get(i).unwrap();
+            let known_datatypes = known_datatypes_per_ir.get(i).unwrap();
+            result.push(IrInspectResult {
+                instruction: format!("{:?}", instruction),
+                statements: Some(statements.iter().map(|s| format!("{:?}", s)).collect()),
+                data_accesses: Some(data_access.iter().map(|s| format!("{:?}", s)).collect()),
+                data_access_per_ir: Some(
+                    known_datatypes.iter().map(|s| format!("{:?}", s)).collect(),
+                ),
+            });
+        } else {
+            result.push(IrInspectResult {
+                instruction: format!("{:?}", instruction),
+                statements: None,
+                data_accesses: None,
+                data_access_per_ir: None,
+            });
+        }
+    }
+    Ok(result)
 }
 
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet, open_file, decom_from_entry])
+        .invoke_handler(tauri::generate_handler![
+            decom_from_address,
+            open_file,
+            decom_from_entry,
+            ir_inspect
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
