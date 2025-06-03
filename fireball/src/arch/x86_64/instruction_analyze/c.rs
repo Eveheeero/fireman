8ea9092320bed0a2c35ccd4a3852b80fba962099
Eveheeero <xhve00000@naver.com>
2025-06-03 06:59:45 +0000

    cmps

--- fireball/src/arch/x86_64/instruction_analyze/c.rs
+++ fireball/src/arch/x86_64/instruction_analyze/c.rs
@@ -20,3 +20,49 @@ pub(super) fn cmp() -> &'static [IrStatement] {
     let calc_flags = calc_flags_automatically(sub, o1_size(), &[&of, &sf, &zf, &af, &cf, &pf]);
     [calc_flags].into()
 }
+
+#[box_to_static_reference]
+pub(super) fn cmps() -> &'static [IrStatement] {
+    let source = d(rsi.clone());
+    let destination = d(rdi.clone());
+    let sub = b::sub(source, u::sign_extend(destination));
+    let calc_flags =
+        calc_flags_automatically(sub, size_architecture(), &[&of, &sf, &zf, &af, &cf, &pf]);
+    [calc_flags].into()
+}
+#[box_to_static_reference]
+pub(super) fn cmpsb() -> &'static [IrStatement] {
+    let source = d(rsi.clone());
+    let destination = d(rdi.clone());
+    let sub = b::sub(source, u::sign_extend(destination));
+    let calc_flags =
+        calc_flags_automatically(sub, size_result_byte(c(1)), &[&of, &sf, &zf, &af, &cf, &pf]);
+    [calc_flags].into()
+}
+#[box_to_static_reference]
+pub(super) fn cmpsw() -> &'static [IrStatement] {
+    let source = d(rsi.clone());
+    let destination = d(rdi.clone());
+    let sub = b::sub(source, u::sign_extend(destination));
+    let calc_flags =
+        calc_flags_automatically(sub, size_result_byte(c(2)), &[&of, &sf, &zf, &af, &cf, &pf]);
+    [calc_flags].into()
+}
+#[box_to_static_reference]
+pub(super) fn cmpsd() -> &'static [IrStatement] {
+    let source = d(rsi.clone());
+    let destination = d(rdi.clone());
+    let sub = b::sub(source, u::sign_extend(destination));
+    let calc_flags =
+        calc_flags_automatically(sub, size_result_byte(c(4)), &[&of, &sf, &zf, &af, &cf, &pf]);
+    [calc_flags].into()
+}
+#[box_to_static_reference]
+pub(super) fn cmpsq() -> &'static [IrStatement] {
+    let source = d(rsi.clone());
+    let destination = d(rdi.clone());
+    let sub = b::sub(source, u::sign_extend(destination));
+    let calc_flags =
+        calc_flags_automatically(sub, size_result_byte(c(8)), &[&of, &sf, &zf, &af, &cf, &pf]);
+    [calc_flags].into()
+}
