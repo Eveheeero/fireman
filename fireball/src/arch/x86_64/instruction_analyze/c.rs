a45626939f715fe2795329fff03b14c18c7ddb22
Eveheeero <xhve00000@naver.com>
2025-06-04 06:52:44 +0000

    clc, cld, cmc, cmpxchg

--- fireball/src/arch/x86_64/instruction_analyze/c.rs
+++ fireball/src/arch/x86_64/instruction_analyze/c.rs
@@ -66,3 +66,108 @@ pub(super) fn cmpsq() -> &'static [IrStatement] {
         calc_flags_automatically(sub, size_result_byte(c(8)), &[&of, &sf, &zf, &af, &cf, &pf]);
     [calc_flags].into()
 }
+
+#[box_to_static_reference]
+pub(super) fn clc() -> &'static [IrStatement] {
+    let set_cl = assign(c(0), cl.clone(), size_relative(cl.clone()));
+    [set_cl].into()
+}
+#[box_to_static_reference]
+pub(super) fn cld() -> &'static [IrStatement] {
+    let set_df = assign(c(0), df.clone(), size_relative(df.clone()));
+    [set_df].into()
+}
+
+#[box_to_static_reference]
+pub(super) fn cmc() -> &'static [IrStatement] {
+    let set_cl = assign(u::not(cl.clone()), cl.clone(), size_relative(cl.clone()));
+    [set_cl].into()
+}
+
+#[box_to_static_reference]
+pub(super) fn cbw() -> &'static [IrStatement] {
+    let set_ax = assign(
+        u::sign_extend(al.clone()),
+        ax.clone(),
+        size_relative(ax.clone()),
+    );
+    [set_ax].into()
+}
+#[box_to_static_reference]
+pub(super) fn cwde() -> &'static [IrStatement] {
+    let set_ax = assign(
+        u::sign_extend(ax.clone()),
+        eax.clone(),
+        size_relative(eax.clone()),
+    );
+    [set_ax].into()
+}
+#[box_to_static_reference]
+pub(super) fn cdqe() -> &'static [IrStatement] {
+    let set_ax = assign(
+        u::sign_extend(eax.clone()),
+        rax.clone(),
+        size_relative(rax.clone()),
+    );
+    [set_ax].into()
+}
+
+#[box_to_static_reference]
+pub(super) fn cmpxchg() -> &'static [IrStatement] {
+    let cond = b::equal(rax.clone(), d(o1()), o1_size());
+    let true_b = [assign(o2(), d(o1()), o1_size())];
+    let false_b = [assign(d(o1()), rax.clone(), o1_size())];
+    let cmpxchg = condition(cond.clone(), true_b, false_b);
+    let calc_flags = calc_flags_automatically(
+        cond,
+        size_result_byte(c(1)),
+        &[&of, &sf, &zf, &af, &cf, &pf],
+    );
+    [calc_flags, cmpxchg].into()
+}
+
+#[box_to_static_reference]
+pub(super) fn cwd() -> &'static [IrStatement] {
+    let set_tmp = assign(
+        u::sign_extend(ax.clone()),
+        tmp32.clone(),
+        size_relative(tmp32.clone()),
+    );
+    let set_dx = assign(
+        b::shr(tmp32.clone(), c(16)),
+        dx.clone(),
+        size_relative(dx.clone()),
+    );
+    let set_ax = assign(tmp32.clone(), ax.clone(), size_relative(ax.clone()));
+    [set_tmp, set_dx, set_ax].into()
+}
+#[box_to_static_reference]
+pub(super) fn cdq() -> &'static [IrStatement] {
+    let set_tmp = assign(
+        u::sign_extend(eax.clone()),
+        tmp64.clone(),
+        size_relative(tmp64.clone()),
+    );
+    let set_dx = assign(
+        b::shr(tmp64.clone(), c(16)),
+        edx.clone(),
+        size_relative(edx.clone()),
+    );
+    let set_ax = assign(tmp64.clone(), eax.clone(), size_relative(eax.clone()));
+    [set_tmp, set_dx, set_ax].into()
+}
+#[box_to_static_reference]
+pub(super) fn cqo() -> &'static [IrStatement] {
+    let set_tmp = assign(
+        u::sign_extend(rax.clone()),
+        tmp128.clone(),
+        size_relative(tmp128.clone()),
+    );
+    let set_dx = assign(
+        b::shr(tmp128.clone(), c(16)),
+        rdx.clone(),
+        size_relative(rdx.clone()),
+    );
+    let set_ax = assign(tmp128.clone(), rax.clone(), size_relative(rax.clone()));
+    [set_tmp, set_dx, set_ax].into()
+}
