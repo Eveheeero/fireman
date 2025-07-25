3fb5c5c46238b4e8f49060ad2b3816c8042fef99
Eveheeero <xhve00000@naver.com>
2025-07-26 01:19:33 +0900

    collapse_unused_variables wip

--- fireball/src/abstract_syntax_tree/optimize/collapse_unused_varaible.rs
+++ fireball/src/abstract_syntax_tree/optimize/collapse_unused_varaible.rs
@@ -1,9 +1,15 @@
-use crate::{
-    abstract_syntax_tree::{Ast, AstFunctionId, AstFunctionVersion, AstVariableId},
-    prelude::DecompileError,
-};
 use hashbrown::HashSet;
 
+use crate::{
+    abstract_syntax_tree::{
+        Ast, AstFunctionId, AstFunctionVersion, AstStatement, AstVariableId, WrappedAstStatement,
+    },
+    ir::data::IrData,
+    prelude::DecompileError,
+    utils::Aos,
+};
+
+/// check variables are overwritten without reading in ir level
 pub(super) fn collapse_unused_variables(
     ast: &mut Ast,
     function_id: AstFunctionId,
@@ -24,8 +30,47 @@ pub(super) fn collapse_unused_variables(
         ir_function = function.ir.clone();
     }
 
-    let used_vars: HashSet<AstVariableId> = HashSet::new();
-    for ast_statement in body.iter().rev() {}
+    let mut used_locations: HashSet<Aos<IrData>> = HashSet::new();
+    let mut new_body: Vec<WrappedAstStatement> = Vec::new();
+    for stmt in body.into_iter().rev() {
+        match stmt.statement {
+            AstStatement::Declaration(ast_variable, wrapped) => todo!(),
+            AstStatement::Assignment(wrapped, wrapped1) => todo!(),
+            AstStatement::If(wrapped, wrapped_ast_statements, wrapped_ast_statements1) => todo!(),
+            AstStatement::While(wrapped, wrapped_ast_statements) => todo!(),
+            AstStatement::For(
+                wrapped_ast_statement,
+                wrapped,
+                wrapped_ast_statement1,
+                wrapped_ast_statements,
+            ) => todo!(),
+            AstStatement::Call(ast_jump_target, wrappeds) => todo!(),
+            AstStatement::Label(_) => todo!(),
+            AstStatement::Block(wrapped_ast_statements) => todo!(),
+            AstStatement::Assembly(_) => todo!(),
+            AstStatement::Comment(_) => todo!(),
+            AstStatement::Ir(ir_statement) => todo!(),
+            AstStatement::Empty => todo!(),
+
+            /* assignment */
+            // variables.get(variable_id).unwrap().data_access_ir.unwrap() check if data access is single and access_type is write
+
+            /* statement containable */
+
+            /* etc */
+
+            /* next statements undetectable */
+            AstStatement::Return(_)
+            | AstStatement::Undefined
+            | AstStatement::Goto(_)
+            | AstStatement::Exception(_) => {
+                used_locations.clear();
+                new_body.push(stmt);
+                continue;
+            }
+        }
+    }
+    new_body.reverse();
 
     {
         let mut functions = ast.functions.write().unwrap();
@@ -33,7 +78,7 @@ pub(super) fn collapse_unused_variables(
             .get_mut(&function_id)
             .and_then(|x| x.get_mut(&function_version))
             .unwrap();
-        function.body = body;
+        function.body = new_body;
     }
     Ok(())
 }
