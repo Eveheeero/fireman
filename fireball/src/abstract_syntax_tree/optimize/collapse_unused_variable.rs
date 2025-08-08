cf4e5c42ffec2738057074b22f883cf5f114b296
Eveheeero <xhve00000@naver.com>
2025-08-08 17:15:56 +0900

    collapse_unused_variable wip +
    - add gitignore for jetbrain
    - add ai agent guideline for junie

--- fireball/src/abstract_syntax_tree/optimize/collapse_unused_variable.rs
+++ fireball/src/abstract_syntax_tree/optimize/collapse_unused_variable.rs
@@ -81,7 +81,10 @@ pub(super) fn collapse_unused_variables(
             /* statement containable */
             AstStatement::If(_cond, branch_true, branch_false) => {
                 let Some(branch_false) = branch_false else {
-                    collapse(&mut overwritten_locations, None, branch_true);
+                    collapse(&variables, &mut overwritten_locations, None, branch_true);
+                    if branch_true.is_empty() {
+                        continue;
+                    }
                     new_body.push(stmt);
                     continue;
                 };
@@ -89,7 +92,33 @@ pub(super) fn collapse_unused_variables(
                 // if so, clone used locations and run each branch
                 // make subset reseted locations and newly used locations
                 let b1_undetectable_exist = is_undetectable_statement_exist(branch_true);
+                let mut b1_newly_overwritten = HashSet::new();
+                let mut b1_overwritten_locations = overwritten_locations.clone();
+                collapse(
+                    &variables,
+                    &mut b1_overwritten_locations,
+                    Some(&mut b1_newly_overwritten),
+                    branch_true,
+                );
+
                 let b2_undetectable_exist = is_undetectable_statement_exist(branch_false);
+                let mut b2_newly_overwritten = HashSet::new();
+                let mut b2_overwritten_locations = overwritten_locations;
+                collapse(
+                    &variables,
+                    &mut b2_overwritten_locations,
+                    Some(&mut b2_newly_overwritten),
+                    branch_false,
+                );
+
+                if b1_undetectable_exist && b2_undetectable_exist {
+                    /* overwritten_locations is set to subset of both newly overwritten */
+                } else if !b1_undetectable_exist && !b2_undetectable_exist {
+                    /* both branches are detectable */
+                } else {
+                }
+                new_body.push(stmt);
+
                 todo!()
             }
             AstStatement::While(_cond, _stmts) => todo!("same with `for`"),
@@ -97,7 +126,7 @@ pub(super) fn collapse_unused_variables(
                 "if for pattern used, there might be user-defined or optimization variable exists. how should we handle this?"
             ),
             AstStatement::Block(stmts) => {
-                collapse(&mut overwritten_locations, None, stmts);
+                collapse(&variables, &mut overwritten_locations, None, stmts);
                 new_body.push(stmt);
                 continue;
             }
@@ -217,44 +246,113 @@ fn is_undetectable_statement_exist(stmts: &[WrappedAstStatement]) -> bool {
     false
 }
 
+/// stmts containable stmt handling is different
 fn collapse(
+    variables: &ArcAstVariableMap,
     overwritten_locations: &mut HashSet<Aos<IrData>>,
-    newly_overwritten: Option<&mut HashSet<Aos<IrData>>>,
+    mut newly_overwritten: Option<&mut HashSet<Aos<IrData>>>,
     stmts: &mut Vec<WrappedAstStatement>,
 ) {
-    enum Action {
-        Nothing,
-        Remove,
-    }
-    for i in (0..stmts.len()).rev() {
+    let mut i = stmts.len();
+    while i > 0 {
+        i -= 1;
+        let mut drop_needed = false;
         let stmt = &mut stmts[i];
+
         match &mut stmt.statement {
             /* removable */
-            AstStatement::Declaration(ast_variable, wrapped) => todo!(),
-            AstStatement::Assignment(wrapped, wrapped1) => todo!(),
+            AstStatement::Declaration(lhs, _rhs) => {
+                let data_access_count: usize = lhs
+                    .data_access_ir
+                    .as_ref()
+                    .unwrap()
+                    .values()
+                    .map(|x| x.len())
+                    .sum();
+                let var_id = &lhs.id;
+                let location = var_id_to_location(&variables, *var_id);
+                let overwritten = overwritten_locations.contains(&location);
+                if data_access_count == 1 && overwritten {
+                    trace!(?lhs,?stmt.comment, "Removing declaration of unused variable");
+                    drop_needed = true;
+                } else {
+                    overwritten_locations.insert(location.clone());
+                    if let Some(newly_overwritten) = &mut newly_overwritten {
+                        newly_overwritten.insert(location);
+                    }
+                }
+            }
+            AstStatement::Assignment(lhs, _rhs) => {
+                if let AstExpression::Variable(_, var_id) = lhs.item {
+                    let location = var_id_to_location(&variables, var_id);
+                    let variables = variables.read().unwrap();
+                    let lhs = variables.get(&var_id).unwrap();
+                    let data_access_count: usize = lhs
+                        .data_access_ir
+                        .as_ref()
+                        .unwrap()
+                        .values()
+                        .map(|x| x.len())
+                        .sum();
+                    let overwritten = overwritten_locations.contains(&location);
+                    if data_access_count == 1 && overwritten {
+                        trace!(?lhs,?stmt.comment, "Removing assignment of unused variable");
+                        drop_needed = true;
+                    } else {
+                        overwritten_locations.insert(location.clone());
+                        if let Some(newly_overwritten) = &mut newly_overwritten {
+                            newly_overwritten.insert(location);
+                        }
+                    }
+                }
+            }
 
             /* stmts containable */
-            AstStatement::If(wrapped, wrapped_ast_statements, wrapped_ast_statements1) => todo!(),
-            AstStatement::While(wrapped, wrapped_ast_statements) => todo!(),
-            AstStatement::For(
-                wrapped_ast_statement,
-                wrapped,
-                wrapped_ast_statement1,
-                wrapped_ast_statements,
-            ) => todo!(),
-            AstStatement::Block(wrapped_ast_statements) => todo!(),
+            AstStatement::If(_cond, branch_true, branch_false) => {
+                if let Some(branch_false) = branch_false {
+                } else {
+                    collapse(
+                        &variables,
+                        overwritten_locations,
+                        newly_overwritten.as_deref_mut(),
+                        branch_true,
+                    );
+                }
+            }
+            AstStatement::While(_cond, _stmts) => todo!("same with `for`"),
+            AstStatement::For(_init, _cond, _update, _stmts) => todo!(
+                "if for pattern used, there might be user-defined or optimization variable exists. how should we handle this?"
+            ),
+            AstStatement::Block(stmts) => {
+                collapse(
+                    variables,
+                    overwritten_locations,
+                    newly_overwritten.as_deref_mut(),
+                    stmts,
+                );
+                if stmts.is_empty() {
+                    drop_needed = true;
+                }
+            }
 
             /* etc */
-            AstStatement::Return(_)
-            | AstStatement::Call(_, _)
-            | AstStatement::Label(_)
+            AstStatement::Label(_) | AstStatement::Comment(_) | AstStatement::Empty => {}
+
+            /* next statements undetectable */
+            AstStatement::Call(_, _)
             | AstStatement::Goto(_)
             | AstStatement::Assembly(_)
-            | AstStatement::Undefined
-            | AstStatement::Exception(_)
-            | AstStatement::Comment(_)
             | AstStatement::Ir(_)
-            | AstStatement::Empty => {}
+            | AstStatement::Return(_)
+            | AstStatement::Undefined
+            | AstStatement::Exception(_) => {
+                overwritten_locations.clear();
+                // newly overwritten won't cleared, it will used in out of recursive calls
+            }
+        }
+
+        if drop_needed {
+            stmts.remove(i);
         }
     }
 }
