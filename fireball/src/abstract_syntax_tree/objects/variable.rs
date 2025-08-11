71320a4f4ebf61da109c402c81011f1b6b95522e
Eveheeero <xhve00000@naver.com>
2025-08-12 03:32:41 +0900

    remove IrStatementDescriptorMap from debug print

--- fireball/src/abstract_syntax_tree/objects/variable.rs
+++ fireball/src/abstract_syntax_tree/objects/variable.rs
@@ -2,8 +2,9 @@ use crate::{
     abstract_syntax_tree::objects::*,
     ir::{data::IrDataAccess, utils::IrStatementDescriptorMap},
 };
+use std::fmt::Formatter;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct AstVariable {
     pub name: Option<String>,
     pub id: AstVariableId,
@@ -11,6 +12,23 @@ pub struct AstVariable {
     pub const_value: Option<Wrapped<AstValue>>,
     /// None if origin is not ir
     pub data_access_ir: Option<IrStatementDescriptorMap<Vec<IrDataAccess>>>,
+}
+
+impl std::fmt::Debug for AstVariable {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "AstVariable {{ name: {:?}, id: {:?}, type: {:?}{}}}",
+            self.name(),
+            self.id,
+            self.var_type,
+            if let Some(val) = &self.const_value {
+                format!(", const_value: {:?}", val)
+            } else {
+                String::new()
+            }
+        )
+    }
 }
 
 impl AstVariable {
